b37ec9846b401d709cd218291661a5cd52bdb9de
schou <pschou@users.noreply.github.com>
2023-02-26 22:49:12 -0500

    add Int64 to rates for per second rates

--- Convert.go
+++ Convert.go
@@ -1,10 +1,26 @@
 package bunit
 
-import "github.com/cymertek/go-big"
+import (
+	"time"
+
+	"github.com/cymertek/go-big"
+)
 
 func (b Bytes) Int64() int64 {
 	return (&big.Int{}).SetBytes(b).Int64()
 }
 func (b Bits) Int64() int64 {
 	return (&big.Int{}).SetBytes(b).Int64()
+}
+func (b BitRate) Int64() int64 {
+	v := (&big.Float{}).SetBytes(b.n, []byte{})
+	v.Mul(v, big.NewFloat(float64(time.Second)/float64(b.d)))
+	r, _ := v.Int64()
+	return r
+}
+func (b ByteRate) Int64() int64 {
+	v := (&big.Float{}).SetBytes(b.n, []byte{})
+	v.Mul(v, big.NewFloat(float64(time.Second)/float64(b.d)))
+	r, _ := v.Int64()
+	return r
 }
