545b11760718ce78cdada5d5722ba8f2398a392d
schou <pschou@users.noreply.github.com>
2023-02-26 22:50:21 -0500

    add Int64 to rates for per second rates

--- Convert.go
+++ Convert.go
@@ -6,18 +6,25 @@ import (
 	"github.com/cymertek/go-big"
 )
 
+// Get the int64 value of the bytes, may be cut off for large values
 func (b Bytes) Int64() int64 {
 	return (&big.Int{}).SetBytes(b).Int64()
 }
+
+// Get the int64 value of the bits, may be cut off for large values
 func (b Bits) Int64() int64 {
 	return (&big.Int{}).SetBytes(b).Int64()
 }
+
+// Get the int64 value of the bit rate per second, may be cut off for large values
 func (b BitRate) Int64() int64 {
 	v := (&big.Float{}).SetBytes(b.n, []byte{})
 	v.Mul(v, big.NewFloat(float64(time.Second)/float64(b.d)))
 	r, _ := v.Int64()
 	return r
 }
+
+// Get the int64 value of the byte rate per second, may be cut off for large values
 func (b ByteRate) Int64() int64 {
 	v := (&big.Float{}).SetBytes(b.n, []byte{})
 	v.Mul(v, big.NewFloat(float64(time.Second)/float64(b.d)))
